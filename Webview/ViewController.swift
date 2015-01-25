ac7e35926056749d9cd42b49eb2aa99760ca2fb7
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-24 22:28:56 -0800

    Add basic webview usage

--- Webview/ViewController.swift
+++ Webview/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myWebview: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var urlPath = "http://www.cafesuda.com"
+        var url = NSURL(string: urlPath)
+        var request = NSURLRequest(URL: url!)
+        myWebview.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
